cefb113f3e3dd384ba2874127d3daaa4b5f8d4c3
mohiey <mohamedmohiey507@gmail.com>
2025-06-30 14:12:37 +0300

    Implemented Content Based Dataset

--- src/data/datasets.py
+++ src/data/datasets.py
@@ -17,3 +17,28 @@ class CollaborativeDataset(Dataset):
             'movie': self.movies[idx],
             'rating': self.ratings[idx]
         }
+
+
+class ContentDataset(Dataset):
+    def __init__(self, df, u2i, m2i, genres_mat, tags_mat, movie_avg, user_avg):
+        self.user_idx  = torch.tensor(df["userId"].map(u2i).values, dtype=torch.long)
+        self.movie_idx = torch.tensor(df["movieId"].map(m2i).values, dtype=torch.long)
+        self.rating    = torch.tensor(df["rating"].values, dtype=torch.float32)
+        self.genres_mat = genres_mat
+        self.tags_mat   = tags_mat
+        self.movie_avg  = movie_avg
+        self.user_avg   = user_avg
+
+    def __len__(self):
+        return len(self.rating)
+
+    def __getitem__(self, idx):
+        m_idx = self.movie_idx[idx]
+        return {
+            'user':      self.user_idx[idx],
+            'genres':    self.genres_mat[m_idx],
+            'tags':      self.tags_mat[m_idx],
+            'movie_avg': self.movie_avg[m_idx],
+            'user_avg':  self.user_avg[self.user_idx[idx]],
+            'rating':    self.rating[idx]
+        }
